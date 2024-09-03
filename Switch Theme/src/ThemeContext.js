7231c8172703b4fe753e83f4e4871a54fc8679a8
daleale <daleale.se@gmail.com>
2024-09-02 22:36:42 -0300

    first commit

---
+++ Switch Theme/src/ThemeContext.js
@@ -0,0 +1,15 @@
+import { useContext, createContext, useState } from "react";
+
+const ThemeContext = createContext()
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light")
+  
+    const toggleTheme = () => {
+        theme === "light" ? setTheme("dark") : setTheme("light")
+    }
+
+    return <ThemeContext.Provider value={{theme, toggleTheme}}>{ children }</ThemeContext.Provider>
+}
+
+export const useTheme = () => useContext(ThemeContext);
